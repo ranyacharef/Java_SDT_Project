5ed6950f39723808a8352816606f5fcb8a2cfc1a
ranyacharef <rania_charef@hotmail.fr>
2020-04-19 20:41:27 +0200

    First commit

---
+++ src/java_project_sdt/Java_Project_SDT.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_project_sdt;
+
+/**
+ *
+ * @author USER
+ */
+public class Java_Project_SDT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
